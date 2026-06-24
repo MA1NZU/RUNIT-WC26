d3c159c923f90cae251be0d98a1284897e0c66c3
MA1NZU <yahyaayman2006@gmail.com>
2026-06-24 23:29:49 +0300

    Update predict.js

--- js/predict.js
+++ js/predict.js
@@ -301,7 +301,7 @@ function renderMatches(rounds, predMap) {
       if (isFinished) {
         statusHtml = `
           <div class="match-date" style="color:var(--green)">
-            🟢 Live / Final &nbsp;|&nbsp;
+            🟢 Live &nbsp;|&nbsp;
             <strong>${match.home_score} – ${match.away_score}</strong>
           </div>`;
       } else if (hasDate) {
